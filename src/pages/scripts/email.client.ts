c897c6f7d9f460a2dc6d27b457af97a4ab20245e
kauehenrick <kauehenrick@proton.me>
2026-04-01 22:10:10 -0300

    feat: changes on copy

---
+++ src/pages/scripts/email.client.ts
@@ -0,0 +1,29 @@
+import emailjs from "@emailjs/browser";
+import { Notyf } from "notyf";
+
+var notyf = new Notyf();
+
+emailjs.init(import.meta.env.PUBLIC_EMAILJS_PUBLIC_KEY);
+
+const form = document.getElementById(
+  "contact-form"
+) as HTMLFormElement | null;
+
+if (form) {
+  form.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    try {
+      await emailjs.sendForm(
+        import.meta.env.PUBLIC_EMAILJS_SERVICE_ID,
+        import.meta.env.PUBLIC_EMAILJS_TEMPLATE_ID,
+        form
+      );
+
+      form.reset();
+      notyf.success("Email enviado");
+    } catch (err) {
+      notyf.error("Erro ao enviar email");
+    }
+  });
+}
